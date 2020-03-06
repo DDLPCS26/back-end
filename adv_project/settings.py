c265075ab607b42663797e2ef3812490d231c8ee
Ian Schwartz <ian.schwartz23@gmail.com>
2020-03-06 10:20:21 -0800

    settings.py update

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 from decouple import config
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -165,6 +166,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-import django_heroku
+
 django_heroku.settings(locals())
 # del DATABASES['default']['OPTIONS']['sslmode']
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)

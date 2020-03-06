27263abc6060fb17b1c13a4ef74f6b444f0f7ea4
Ian Schwartz <ian.schwartz23@gmail.com>
2020-03-06 08:45:57 -0800

    another option settings.py

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -97,9 +97,19 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 #     }
 # }
 
-DATABASES = {}
+# DATABASES = {}
 
-DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
+# DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
